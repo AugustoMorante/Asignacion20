6a05c267387945c82ab568d93a121f1c64e240ee
Augusto Morante <48858576+AugustoMorante@users.noreply.github.com>
2019-06-18 21:11:33 -0500

    Add files via upload

---
+++ Hoja1_Ejercicio2.cpp
@@ -0,0 +1,48 @@
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	int m, n;
+	bool simetrica;
+	int**matriz;
+	while (1)
+	{
+		do
+		{
+			cout << "Inserte m: "; cin >> m;
+		} while (m <= 0 || m >= 10);
+		do
+		{
+			cout << "Inserte n: "; cin >> n;
+		} while (n <= 0 || n >= 10);
+		cout << endl;
+		matriz = new int*[n];
+		for (int i = 0; i < n; i++)
+		{
+			matriz[i] = new int[m];
+			for (int j = 0; j < m; j++)
+			{
+				if (i == j)
+				{
+					simetrica = 1;
+				}
+				else
+				{
+					simetrica = 0;
+				}
+			}
+		}
+
+		if (simetrica == true)
+			cout << "Si es simetrica" << endl;
+		else
+			cout << "No es simetrica" << endl;
+		cout << endl;
+	}
+	_getch();
+}
